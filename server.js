141283f74925422c109f19e5fe8c56d310a11b8a
James Jackson <jjackson0228@gmail.com>
2024-08-14 13:23:44 -0400

    added middleware to server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const { Pool } = require('pg');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Connect to database
 const pool = new Pool(
   {
