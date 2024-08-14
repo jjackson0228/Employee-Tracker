7b5008a82a6e1544db853e3ff5182498114a7325
James Jackson <jjackson0228@gmail.com>
2024-08-14 13:29:56 -0400

    added some departments in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (name)
+VALUES  ('Sales'),
+        ('Repairs'),
+        ('Electric Guitars Specialist'),
+        ('Drums Specialist'),
+        ('Audio Equipment Specialist'),
+        ('Bass Guitars Specialist'),
+        ('Acoustic Guitars Specialist'),
+        ('Lesson Instructors');
+
+       
