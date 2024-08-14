509c745833cb47210c7c531da3b4bc64f90961c8
James Jackson <jjackson0228@gmail.com>
2024-08-14 13:18:12 -0400

    added notes to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,15 +10,15 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    title VARCHAR(30) UNIQUE NOT NULL,--hold role title
+    salary DECIMAL NOT NULL, -- hold role salary
+    department_id INTEGER NOT NULL --hold reference to department role belongs to
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    first_name VARCHAR(30) NOT NULL, --hold employee first name
+    last_name VARCHAR(30) NOT NULL, --hold employee last name
+    role_id INTEGER NOT NULL, --hold reference to employee role
+    manager_id INTEGER -- hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 );
