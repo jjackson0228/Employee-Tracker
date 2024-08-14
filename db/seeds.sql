a45103e0ccf919ea67b85a670b87f55ce1b66a69
James Jackson <jjackson0228@gmail.com>
2024-08-14 13:32:29 -0400

    changed the seeds.sql took roles off of departments I went to into detail

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO department (name)
 VALUES  ('Sales'),
         ('Repairs'),
-        ('Electric Guitars Specialist'),
-        ('Drums Specialist'),
-        ('Audio Equipment Specialist'),
-        ('Bass Guitars Specialist'),
-        ('Acoustic Guitars Specialist'),
-        ('Lesson Instructors');
+        ('Electric Guitars'),
+        ('Drums'),
+        ('Audio Equipment'),
+        ('Bass Guitars'),
+        ('Acoustic Guitars'),
+        ('Lessons');
 
        
