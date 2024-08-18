ecef5ca6c889d25812d0a4d0f5b50de1699e9305
James Jackson <jjackson0228@gmail.com>
2024-08-18 14:29:05 -0400

    added role id and employee id to tables of role and employee and fixed the pool.connect in db.js

--- db.js
+++ db.js
@@ -2,16 +2,19 @@
 const { Pool } = require('pg');
 
 // Connect to database
-const pool = new Pool(
-  {
-    // Enter PostgreSQL username, password, host, database, and port
-    user: 'postgres',
-    password: 'swag',
-    host: 'localhost',
-    database: 'employees_db',
-    port: 5432,
-  },
-  console.log(`Connected to the employees_db database.`)
-);
+const pool = new Pool({
+  // Enter PostgreSQL username, password, host, database, and port
+  user: 'postgres',
+  password: 'swag',
+  host: 'localhost',
+  database: 'employees_db',
+  port: 5432,
+});
+
+// Log connection success
+pool
+  .connect()
+  .then(() => console.log('Connected to the employees_db database.'))
+  .catch((err) => console.error('Connection error', err.stack));
 
 module.exports = pool;
--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,13 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,--hold role title
     salary DECIMAL NOT NULL, -- hold role salary
-    department_id INTEGER NOT NULL --hold reference to department role belongs to
+    department_id INTEGER NOT NULL department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL, --hold employee first name
     last_name VARCHAR(30) NOT NULL, --hold employee last name
-    role_id INTEGER NOT NULL, --hold reference to employee role
-    manager_id INTEGER -- hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    role_id INTEGER NOT NULL role(id),--hold reference to employee role
+    manager_id INTEGER employee(id)
 );
