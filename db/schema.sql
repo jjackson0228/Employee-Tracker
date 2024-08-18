9c55a009f0bd523506182f3c3ae8ab11605c05aa
James Jackson <jjackson0228@gmail.com>
2024-08-18 14:58:50 -0400

    added REFERENCES to schema.sql and added notes explaining

--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,13 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,--hold role title
     salary DECIMAL NOT NULL, -- hold role salary
-    department_id INTEGER NOT NULL department(id)
+    department_id INTEGER NOT NULL REFERENCES department(id) --hold reference to department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL, --hold employee first name
     last_name VARCHAR(30) NOT NULL, --hold employee last name
-    role_id INTEGER NOT NULL role(id),--hold reference to employee role
-    manager_id INTEGER employee(id)
+    role_id INTEGER NOT NULL REFERENCES role(id),--hold reference to role(id)
+    manager_id INTEGER REFERENCES employee(id) --hold reference to employee(id)
 );
